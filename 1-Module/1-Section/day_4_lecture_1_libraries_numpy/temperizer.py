6cf8fbd55a25e9494d26d04a1471aecf631150e9
Kyle Hayes <khayes847@gmail.com>
2019-07-18 12:55:31 -0400

    After class

--- 1-Module/1-Section/day_4_lecture_1_libraries_numpy/temperizer.py
+++ 1-Module/1-Section/day_4_lecture_1_libraries_numpy/temperizer.py
@@ -9,4 +9,33 @@ def convert_f_to_c(temperature_f):
 
 def convert_c_to_f(temperature_c):
     """Convert Celsius to Fahrenheit."""
-    pass
+    temperature_f = (temperature_c * 9/5) + 32
+    return temperature_f
+
+def convert_c_to_k(temperature_c):
+    """Convert Celsius to Kelvin."""
+    temperature_k = temperature_c + 273.15
+    return temperature_k
+
+def convert_f_to_k(temperature_f):
+    """Convert Fahrenheit to Kelvin."""
+    temperature_k = (temperature_f + 459.67) * (5/9)
+    return temperature_k
+
+def convert_k_to_c(temperature_k):
+    """Convert Kelvin to Celsius."""
+    temperature_c = temperature_k - 273.15
+    return temperature_c
+
+def convert_k_to_f(temperature_k):
+    """Convert Kelvin to Fahrenheit."""
+    temperature_f = (temperature_k * (9/5)) - 459.67
+    return temperature_f
+
+def convert_f_to_all(temperature_f):
+    """Convert Fahrenheit to All."""
+    temperature_c = (temperature_f - 32) * (5/9)
+    temperature_k = (temperature_f + 459.67) * (5/9)
+    return print(f"Celsius: {temperature_c}. 
+                 \nKelvin: {temperature_k}.")
+    
